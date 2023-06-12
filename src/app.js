629ab96a3c9d27b4fc4363d5d6f21ed17894f043
Maxinator228 <sorochinskiy-2020@mail.ru>
2023-06-12 12:38:02 +0700

    fixed

--- src/app.js
+++ src/app.js
@@ -1,23 +1,39 @@
-const number1Input = document.getElementById('number1');
-    const number2Input = document.getElementById('number2');
-    const resultInput = document.getElementById('result');
+//Тариф//
+function calculateTotal() {
+  let field1Value = parseInt(document.getElementById("field1").value);
+  let field2Value = parseInt(document.getElementById("field2").value);
+  let totalValue = 0;
 
-    // Функция для обновления результата при изменении значений в полях ввода
-    function updateResult() {
-      const number1 = parseFloat(number1Input.value);
-      const number2 = parseFloat(number2Input.value);
-      if (!isNaN(number1) && !isNaN(number2)) {
-        const sum = number1 + number2;
-        resultInput.value = sum.toFixed(2); // Округляем результат до двух знаков после запятой
-      } else {
-        resultInput.value = ''; // Очищаем поле результата, если введены некорректные значения
-      }
+  if (isNaN(field1Value)) {
+    field1Value = 0;
+  }
+
+  if (isNaN(field2Value)) {
+    field2Value = 0;
+  }
+
+  if (field1Value >= 1) {
+    if (field2Value <= 3) {
+      totalValue = field2Value * 2000 * field1Value;
+    } else if (field2Value <= 5) {
+      totalValue = field2Value * 1800 * field1Value;
+    } else if (field2Value >= 5){
+      totalValue = field2Value * 1680 * field1Value;
     }
-    // Обновляем результат при изменении значений в полях ввода
-    number1Input.addEventListener('input', updateResult);
-    number2Input.addEventListener('input', updateResult);
+  }
+
+  document.getElementById("total").value = totalValue + "₸";
+}
+document.addEventListener("DOMContentLoaded", function() {
+  let field1 = document.getElementById("field1");
+  let field2 = document.getElementById("field2");
+
+  field1.addEventListener("input", calculateTotal);
+  field2.addEventListener("input", calculateTotal);
+});
 
 
+// Заявка //
 const emailButton = document.getElementById("email");
 const telegramButton = document.getElementById("telegram");
 const emailInput = document.getElementById("email-input");
@@ -37,3 +53,4 @@ telegramButton.addEventListener("click", () => {
       emailInput.style.display = "none";
     });
         
+d
